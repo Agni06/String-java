4cbde264dd8580f9fb36ad1ee5e0785d544ed4b8
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-04-17 12:51:28 +0530

    Create StringFun.java

---
+++ StringFun.java
@@ -0,0 +1,9 @@
+String s = "AgniCharanTeja";
+System.out.println(s.toupperCase());
+System.out.println(s.toLowerCase());
+System.out.println(s.charAt());
+System.out.println(s.indexOf('C'));
+System.out.println(s.startsWith("Agni"));
+System.out.println(s.endsWith("Teja"));
+System.out.println(s.contains("Charan"));
+System.out.println(s.subString(4, 10));
